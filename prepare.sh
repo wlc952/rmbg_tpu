83c09c710d3746d9afb63fb3db6000307d9b06c8
王连超 <wlc@zju.edu.cn>
2024-11-14 15:06:58 +0800

    Update prepare.sh

--- prepare.sh
+++ prepare.sh
@@ -10,5 +10,14 @@ pip3 install -r requirements.txt -i https://pypi.tuna.tsinghua.edu.cn/simple
 ## 安装tpu_perf
 if ! python3 -c "import tpu_perf" &> /dev/null; then
     echo "tpu_perf could not be found, installing..."
-    pip3 install https://modelscope.cn/models/wlc952/aigchub_models/resolve/master/tpu_perf-1.2.60-py3-none-manylinux2014_aarch64.whl
+    # 检查系统架构并安装相应的包
+    ARCH=$(uname -m)
+    if [ "$ARCH" == "x86_64" ]; then
+        pip3 install https://modelscope.cn/models/wlc952/aigchub_models/resolve/master/tpu_perf-1.2.60-py3-none-manylinux2014_x86_64.whl
+    elif [ "$ARCH" == "aarch64" ]; then
+        pip3 install https://modelscope.cn/models/wlc952/aigchub_models/resolve/master/tpu_perf-1.2.60-py3-none-manylinux2014_aarch64.whl
+    else
+        echo "不支持的系统架构: $ARCH"
+        exit 1
+    fi
 fi
